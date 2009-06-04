11b297c1931dff0e7094f9f16a39f410e8c7f156
jefflund <jefflund@42e16e12-41d0-11de-b6cc-f32266f7ba4a>
2009-06-04 19:34:02 +0000

    Removed redundant Bresenham code

--- jade/path/Bresenham.java
+++ jade/path/Bresenham.java
@@ -9,51 +9,58 @@ public class Bresenham implements Path
 {
 	public List<Coord> getPath(World world, Coord start, Coord goal)
 	{
-		int x1 = start.x();
-		int y1 = start.y();
-		int x2 = goal.x();
-		int y2 = goal.y();
+		List<Coord> path = castray(world, start.x(), start.y(), goal.x(), goal.y());
+		if(path.get(path.size() - 1).equals(goal))
+			return path;
+		else
+			return null;
+	}
+
+	public List<Coord> castray(World world, int x1, int y1, int x2, int y2)
+	{
 		List<Coord> path = new LinkedList<Coord>();
-		boolean steep = Math.abs(y2 - y1) > Math.abs(x2 - x1);
-		if(steep)
+		path.add(new Coord(x1, y1));
+		int dx = Math.abs(x2 - x1) << 1;
+		int dy = Math.abs(y2 - y1) << 1;
+		int ix = x2 > x1 ? 1 : -1;
+		int iy = y2 > y1 ? 1 : -1;
+		if(dx >= dy)
 		{
-			int temp = x1;
-			x1 = y1;
-			y1 = temp;
-			temp = x2;
-			x2 = y2;
-			y2 = temp;
-		}
-		if(x1 > x2)
-		{
-			int temp = x1;
-			x1 = x2;
-			x2 = temp;
-			temp = y1;
-			y1 = y2;
-			y2 = temp;
-		}
-		int dx = x2 - x1;
-		int dy = Math.abs(y2 - y1);
-		int err = dx / 2;
-		int y = y1;
-		int ystep = y1 < y2 ? 1 : -1;
-		for(int x = x1; x <= x2; x++)
-		{
-			Coord coord = steep? new Coord(y, x) : new Coord(x, y);
-			if(world.passable(coord))
-				path.add(coord);
-			else
-				return null;
-			err = err - dy;
-			if(err < 0)
+			int error = dy - (dx >> 1);
+			while(x1 != x2)
 			{
-				y = y + ystep;
-				err = err + dx;
+				if(error >= 0 && (error != 0 || ix > 0))
+				{
+					y1 += iy;
+					error -= dx;
+				}
+				x1 += ix;
+				error += dy;
+				path.add(new Coord(x1, y1));
+				if(!world.passable(x1, y1))
+					break;
+			}
+		}
+		else
+		// dx < dy
+		{
+			int error = dx - (dy >> 1);
+			while(y1 != y2)
+			{
+				if(error >= 0 && (error != 0 || iy > 0))
+				{
+					x1 += ix;
+					error -= dy;
+				}
+				y1 += iy;
+				error += dx;
+				path.add(new Coord(x1, y1));
+				if(!world.passable(x1, y1))
+					break;
 			}
 		}
 		return path;
-	}
+	}// end castray
 
 	public boolean hasPath(World world, Coord start, Coord goal)
 	{
