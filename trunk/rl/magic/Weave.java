f7c76951c23f283d73f57b418c75120464d8045e
jefflund <jefflund@42e16e12-41d0-11de-b6cc-f32266f7ba4a>
2009-06-06 04:31:31 +0000

    spells added.

--- trunk/rl/magic/Weave.java
+++ trunk/rl/magic/Weave.java
@@ -5,12 +5,18 @@ import java.awt.Color;
 
 public class Weave extends Actor
 {
-	public Weave()
+	private int duration;
+	
+	public Weave(int duration)
 	{
 		super('*', Color.red);
+		this.duration = duration;
 	}
 	
 	public void act()
 	{
+		duration--;
+		if(duration == 0)
+			expire();
 	}
 }
