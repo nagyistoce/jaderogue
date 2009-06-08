b04ac7486be1ba6c3bd44c11f7914468bfb1f762
jefflund <jefflund@42e16e12-41d0-11de-b6cc-f32266f7ba4a>
2009-06-08 05:45:29 +0000

    Fixing for the week

--- trunk/rl/item/Item.java
+++ trunk/rl/item/Item.java
@@ -5,12 +5,25 @@ import java.awt.Color;
 
 public class Item extends Actor
 {
-	public Item(char face, Color color)
+	public enum Slot 
+	{
+		Weapon, Armor
+	};
+	
+	private Slot slot;
+	
+	public Item(char face, Color color, Slot slot)
 	{
 		super(face, color);
+		this.slot = slot;
 	}
 
 	public void act()
 	{
+	}
+	
+	public Slot slot()
+	{
+		return slot;
 	}
 }
