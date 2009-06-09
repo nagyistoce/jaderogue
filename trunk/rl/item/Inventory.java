a9c263401b75ee30546ccc2b50bd011207fc9351
jefflund <jefflund@42e16e12-41d0-11de-b6cc-f32266f7ba4a>
2009-06-09 13:35:10 +0000

    Inventory

---
+++ trunk/rl/item/Inventory.java
@@ -0,0 +1,81 @@
+package rl.item;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import rl.creature.Creature;
+import rl.item.Item.Slot;
+
+public class Inventory
+{
+	private Creature owner;
+	private List<Item> inventory;
+	private Map<Slot, Item> equipment;
+	
+	public Inventory(Creature owner)
+	{
+		this.owner = owner;
+		inventory = new LinkedList<Item>();
+		equipment = new HashMap<Slot, Item>();
+	}
+
+	public List<Item> getEquiped()
+	{
+		return new LinkedList<Item>(equipment.values());
+	}
+	
+	public List<Item> getItems()
+	{
+		return inventory;
+	}
+	
+	public void equip(Item item)
+	{
+		if(item == null)
+			owner.appendMessage("Invalid selection");
+		else
+		{
+			if(equipment.get(item.slot()) != null)
+				owner.appendMessage(equipment.get(item.slot()) + " already equiped");
+			else
+			{
+				inventory.remove(item);
+				equipment.put(item.slot(), item);
+				owner.appendMessage(item + " equiped");
+			}
+		}
+	}
+
+	public void unequip(Item item)
+	{
+		if(item == null)
+			owner.appendMessage("Invalid selection");
+	}
+
+	public void get()
+	{
+		Item item = (Item)owner.world()
+		    .getActorAt(owner.x(), owner.y(), Item.class);
+		if(item != null)
+		{
+			item.attachTo(owner);
+			inventory.add(item);
+			owner.appendMessage(owner + " picks up " + item);
+		}
+		else
+			owner.appendMessage("Nothing to pick up");
+	}
+
+	public void drop(Item item)
+	{
+		if(item == null)
+			owner.appendMessage("Invalid selection");
+		else
+		{
+			item.detachFrom();
+			inventory.remove(item);
+			owner.appendMessage(owner + " drops " + item);
+		}
+	}
+}
