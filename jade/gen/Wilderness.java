733664f2231b5ce9e959bb5dcc276831480d74cd
jefflund <jefflund@42e16e12-41d0-11de-b6cc-f32266f7ba4a>
2009-06-20 15:17:29 +0000

    Refactoring.

--- jade/gen/Wilderness.java
+++ jade/gen/Wilderness.java
@@ -18,10 +18,14 @@ public class Wilderness implements Gen
 	private static final Color OPEN_COLOR1 = Color.white;
 	private static final Color OPEN_COLOR2 = Color.green;
 	private static final float TREE_CHANCE = .05f;
+	private static final int MAX_ROOM_SIZE = 9;
+	private static final int MIN_ROOM_SIZE = 3;
+	private final boolean rooms;
 
-	protected Wilderness()
+	protected Wilderness(boolean rooms)
 	{
 		dice = new Dice();
+		this.rooms = rooms;
 	}
 
 	public void generate(World world, long seed)
@@ -49,5 +53,22 @@ public class Wilderness implements Gen
 			world.tile(0, y).setTile('#', OPEN_COLOR1, false);
 			world.tile(world.width - 1, y).setTile('#', OPEN_COLOR1, false);
 		}
+		if(rooms)
+			addRooms(world);
+	}
+	
+	private void addRooms(World world)
+	{
+		for(int x = 0; x + MAX_ROOM_SIZE < world.width - 2; x += MAX_ROOM_SIZE)
+			for(int y = 0; y + MAX_ROOM_SIZE < world.height - 2; y += MAX_ROOM_SIZE)
+			{
+				int x1 = dice.nextInt(x + 1, x + MAX_ROOM_SIZE - MIN_ROOM_SIZE - 1);
+				int x2 = dice.nextInt(x1 + MIN_ROOM_SIZE - 1, x + MAX_ROOM_SIZE - 1);
+				int y1 = dice.nextInt(y + 1, y + MAX_ROOM_SIZE - MIN_ROOM_SIZE - 1);
+				int y2 = dice.nextInt(y1 + MIN_ROOM_SIZE - 1, y + MAX_ROOM_SIZE - 1);
+				for(int i = x1; i <= x2; i++)
+					for(int j = y1; j <= y2; j++)
+						world.tile(i, j).setTile('#', Color.white, false);
+			}
 	}
 }
