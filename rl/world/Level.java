05340bdff6b8449b716445c01106dec6de86cdd8
jefflund <jefflund@42e16e12-41d0-11de-b6cc-f32266f7ba4a>
2009-07-10 20:05:21 +0000

    mp added.

--- rl/world/Level.java
+++ rl/world/Level.java
@@ -23,7 +23,7 @@ public class Level extends World implements Serializable
 
 	public Level(int depth)
 	{
-		super(80, 23);
+		super(80, 22);
 		Dice random = new Dice(depth);
 		int algorithm = depth == 0 ? GenFactory.Town : GenFactory.Traditional;
 		GenFactory.get(algorithm).generate(this, depth);
