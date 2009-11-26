374f15792608596223413a0e935d318702c542cb
Ido.Yehieli <Ido.Yehieli@42e16e12-41d0-11de-b6cc-f32266f7ba4a>
2009-11-26 19:58:28 +0000

    Added Direction & ExtraTools

---
+++ branches/ido/jade/util/ExtraTools.java
@@ -0,0 +1,51 @@
+package jade.util;
+
+import static java.awt.event.KeyEvent.*;
+
+public class ExtraTools extends Tools {
+	public static Direction keycodeToStep(int keycode) {
+		switch (keycode) {
+		case VK_RIGHT:
+		case VK_KP_RIGHT:
+			return Direction.E;
+		case VK_LEFT:
+		case VK_KP_LEFT:
+			return Direction.W;
+		case VK_UP:
+		case VK_KP_UP:
+			return Direction.N;
+		case VK_DOWN:
+		case VK_KP_DOWN:
+			return Direction.S;
+		default:
+			return null;
+		}
+	}
+
+	static Enum next(Enum[] values, int ordinal, int length) {
+		Enum next = null;
+
+		for (Enum mo : values)
+			if ((ordinal + 1) % length == mo.ordinal())
+				next = mo;
+
+		return next;
+	}
+
+	static Enum prev(Enum[] values, int ordinal, int length) {
+		Enum next = null;
+
+		for (Enum mo : values)
+			if ((ordinal + length - 1) % length == mo.ordinal())
+				next = mo;
+
+		return next;
+	}
+	
+	static int normalize(int i) {
+		if(i==0)
+			return 0;
+		else
+			return i<0?-1:1;
+	}
+}
