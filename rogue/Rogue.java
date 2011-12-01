d3e4c31e7826c21d90fc0790d1f8dea7f8c9b5c1
jefflund <jefflund@42e16e12-41d0-11de-b6cc-f32266f7ba4a>
2011-12-01 21:37:19 +0000

    TiledTermPanel is now complete, although it sorely lacks javadoc and unit test...so much for TDD :(

--- rogue/Rogue.java
+++ rogue/Rogue.java
@@ -1,7 +1,6 @@
 package rogue;
 
 import jade.core.World;
-import jade.ui.Terminal;
 import jade.ui.TiledTermPanel;
 import jade.util.datatype.ColoredChar;
 import java.awt.Color;
@@ -13,7 +12,12 @@ public class Rogue
 {
     public static void main(String[] args) throws InterruptedException
     {
-        Terminal term = TiledTermPanel.getFramedTerminal("Jade Rogue");
+        TiledTermPanel term = TiledTermPanel.getFramedTerminal("Jade Rogue");
+        term.registerTile("dungeon.png", 5, 59, ColoredChar.create('#'));
+        term.registerTile("dungeon.png", 3, 60, ColoredChar.create('.'));
+        term.registerTile("dungeon.png", 5, 20, ColoredChar.create('@'));
+        term.registerTile("dungeon.png", 14, 30, ColoredChar.create('D', Color.red));
+        
         Player player = new Player(term);
         World world = new Level(69, 24, player);
         world.addActor(new Monster(ColoredChar.create('D', Color.red)));
